5c665202a8b0648686a0aef6dddfbe817751cefb
odair <odair.sp@hotmail.com>
2022-07-20 17:41:43 -0300

    atualiza introducao_js

---
+++ ImpulsoJavascript/introducao_js/aulas/primeiras_aulas.js
@@ -0,0 +1,12 @@
+// comentario
+/*comentario bloco*/
+
+const PRECO = 4; 
+var preco = 2;
+var desconto = 0.2;
+
+function soma(a, b) {
+    return a + b;
+}
+console.log("hello word")
+console.log(soma(preco,desconto));
