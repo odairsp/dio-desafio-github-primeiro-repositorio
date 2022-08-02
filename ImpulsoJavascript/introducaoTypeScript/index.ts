9fd86075e67409b7f4e8d545d3f3a7dccb6514b5
odairsp <odair.sp@hotmail.com>
2022-08-02 11:20:34 -0300

    atualiza curso

--- ImpulsoJavascript/introducaoTypeScript/index.ts
+++ ImpulsoJavascript/introducaoTypeScript/index.ts
@@ -1,1 +1,15 @@
-console.log("TypeScript");
+const input = document.getElementById('input') as HTMLInputElement;
+
+input.addEventListener('input', (event) => {
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value);
+})
+
+// Generics types
+
+function apendiceDeLista <T> (array: any[], valor: T){
+    return array.map(item => + valor);
+}
+
+apendiceDeLista([1,2,3],1);
+
