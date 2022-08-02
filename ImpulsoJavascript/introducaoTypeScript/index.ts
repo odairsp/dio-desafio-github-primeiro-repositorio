eadde0bc20f13f462dd3507dc407b196838f6ec6
odairsp <odair.sp@hotmail.com>
2022-08-02 12:22:53 -0300

    Avança curso Typscript

--- ImpulsoJavascript/introducaoTypeScript/index.ts
+++ ImpulsoJavascript/introducaoTypeScript/index.ts
@@ -1,3 +1,13 @@
+import $ from 'jquery';
+
+$.fn.extend({
+    novaFuncao() {
+        console.log('Chamou função!');
+    }
+});
+
+
+
 const input = document.getElementById('input') as HTMLInputElement;
 
 input.addEventListener('input', (event) => {
@@ -13,3 +23,40 @@ function apendiceDeLista <T> (array: any[], valor: T){
 
 apendiceDeLista([1,2,3],1);
 
+
+
+// verificando propriedade com varia interfaces
+
+interface IUsuario{
+    id: string;
+    email: string;
+}
+
+interface IAdmin extends IUsuario{
+    cargo: 'gerente'|'coordenador'|'supervisor';
+}
+
+function redirecione1(usuario: IUsuario | IAdmin){
+    if('cargo' in usuario){
+        // redirecionar para administração
+        
+    // redirecionar para area do usuario
+}
+
+
+// outra possibilidade utilizando o operador ' ? '
+
+interface IUsuario1{
+    id: string;
+    email: string;
+    // '?' define propriedade como opcional
+    cargo?: 'funcionario' | 'gerente'|'coordenador'|'supervisor'; 
+}
+
+function redirecione(usuario: IUsuario1 ){
+    if(usuario.cargo){
+        // redirecionar (usuario.cargo)
+        
+    }
+    // redirecionar para area do usuario
+}
