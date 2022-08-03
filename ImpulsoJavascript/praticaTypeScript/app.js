4455626c3902ef25dd1f939dcf7bc02095363634
odairsp <odair.sp@hotmail.com>
2022-08-03 14:52:53 -0300

    inicia proejeto mentoria typescript

--- ImpulsoJavascript/praticaTypeScript/app.js
+++ ImpulsoJavascript/praticaTypeScript/app.js
@@ -3,13 +3,18 @@ console.log("Estou vinculado ao index.HTML");
 let botao = document.getElementById('button');
 let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
-function somar(num1, num2) {
+function somar(num1, num2, devePrintar, frase) {
+    if (devePrintar) {
+        console.log(frase + (num1 + num2));
+    }
     return num1 + num2;
 }
+let devePrintar = true;
+let frase = "O valor é: ";
 if (botao) {
     botao.addEventListener('click', () => {
         if (input1 && input2) {
-            console.log(somar(Number(input1.value), Number(input2.value)));
+            console.log(somar(Number(input1.value), Number(input2.value), devePrintar, frase));
         }
     });
 }
--- ImpulsoJavascript/praticaTypeScript/app.ts
+++ ImpulsoJavascript/praticaTypeScript/app.ts
@@ -5,14 +5,21 @@ let input1 = document.getElementById('input1') as HTMLInputElement;
 let input2 = document.getElementById('input2') as HTMLInputElement;
 
 
-function somar(num1: number, num2: number) {
+function somar(num1: number, num2: number, devePrintar: boolean, frase: string) {
+    if(devePrintar){
+        console.log(frase+(num1+num2));
+    }
     return num1 + num2;
 }
+
+
+let devePrintar = true;
+let frase = "O valor é: "
 
 if(botao){
     botao.addEventListener('click', () => {
         if(input1 && input2){
-            console.log(somar(Number(input1.value), Number(input2.value)));
+            console.log(somar(Number(input1.value), Number(input2.value), devePrintar, frase));
         }
         
     })
