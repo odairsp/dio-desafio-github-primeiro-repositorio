f3a131f6a5e15890fa6f7a776bf9092469befaa6
odairsp <odair.sp@hotmail.com>
2022-08-03 09:46:10 -0300

    adiciona projeto praticaTypeScript

---
+++ ImpulsoJavascript/praticaTypeScript/app.ts
@@ -0,0 +1,19 @@
+console.log("Estou vinculado ao index.HTML");
+
+let botao = document.getElementById('button');
+let input1 = document.getElementById('input1') as HTMLInputElement;
+let input2 = document.getElementById('input2') as HTMLInputElement;
+
+
+function somar(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+if(botao){
+    botao.addEventListener('click', () => {
+        if(input1 && input2){
+            console.log(somar(Number(input1.value), Number(input2.value)));
+        }
+        
+    })
+}
